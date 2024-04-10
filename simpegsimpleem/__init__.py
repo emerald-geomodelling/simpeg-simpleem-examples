5aa723ee26ad7d2aa33be8c550c461663b801274
Egil <egil.moller@freecode.no>
2024-04-10 10:52:12 +0200

    Removed old stuff

--- simpegsimpleem/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import nadeau_bengio, make_2layer, add_noise, add_uncertainty_normal, add_uncertainty
-from .base import XYZSystem
-from .single import SingleMomentTEMXYZSystem
-from .dual import DualMomentTEMXYZSystem
